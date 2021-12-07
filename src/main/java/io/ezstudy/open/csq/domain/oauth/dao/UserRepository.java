3eacf8a567720bc396ced0faab7b99086aadc65f
JeongYunha <yunhadev98@gmail.com>
2021-12-07 20:28:42 +0900

    feat: spring oauth2 - google login

--- src/main/java/io/ezstudy/open/csq/domain/oauth/dao/UserRepository.java
+++ src/main/java/io/ezstudy/open/csq/domain/oauth/dao/UserRepository.java
@@ -3,5 +3,8 @@ package io.ezstudy.open.csq.domain.oauth.dao;
 import io.ezstudy.open.csq.domain.oauth.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
