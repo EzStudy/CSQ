8b5d3ea0fde163d999f5e2550b86ebd26d7c19ea
JeongYunha <yunhadev98@gmail.com>
2021-12-21 23:53:18 +0900

    feat: role-based authorization

--- src/main/java/io/ezstudy/open/csq/domain/category/api/CategoryApi.java
+++ src/main/java/io/ezstudy/open/csq/domain/category/api/CategoryApi.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,12 +18,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/categories")
-@RestController
+@Controller
 public class CategoryApi {
 
   private final CategoryService categoryService;
+
+  @GetMapping("/newCategory")
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  public String newCategory(){
+    return "newCategory";
+  }
 
   @PostMapping
   @PreAuthorize("hashRole('ROLE_ADMIN')")
@@ -39,7 +48,7 @@ public class CategoryApi {
   }
 
   @GetMapping
-  public ResponseEntity<List<Category>> findAll() {
+  public ResponseEntity<List<Category>> findAll(HttpSession httpSession) {
     List<Category> categoryList = categoryService.findAll();
     HttpStatus status = categoryList != null ? HttpStatus.OK : HttpStatus.NOT_FOUND;
     return new ResponseEntity<>(categoryList, status);
