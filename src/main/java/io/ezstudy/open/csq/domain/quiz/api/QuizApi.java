b0e40a0cce2f3a5ad6ef4b51d865276e6eb7d550
yunhaDevGit <yunhaDev98@gmail.com>
2022-01-27 18:19:31 +0900

    docs: Swagger 적용

--- src/main/java/io/ezstudy/open/csq/domain/quiz/api/QuizApi.java
+++ src/main/java/io/ezstudy/open/csq/domain/quiz/api/QuizApi.java
@@ -2,6 +2,7 @@ package io.ezstudy.open.csq.domain.quiz.api;
 
 import io.ezstudy.open.csq.domain.quiz.application.QuizService;
 import io.ezstudy.open.csq.domain.quiz.domain.Quiz;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Tag(name="Quiz API")
 @RequiredArgsConstructor
 @RequestMapping("/quizes")
 //@RestController
@@ -25,22 +27,26 @@ public class QuizApi {
 
   private final QuizService quizService;
 
+  @Operation(summary = "퀴즈 목록 화면")
   @GetMapping("/list")
   public String quizList(){
     return "quiz/list";
   }
 
+  @Operation(summary = "퀴즈 생성 화면")
   @GetMapping("/createQuiz")
   public String createQuiz(){
     return "quiz/create";
   }
 
+  @Operation(summary = "퀴즈 생성")
   @PostMapping()
   @ResponseStatus(HttpStatus.CREATED)
   public void createQuiz(@RequestBody Quiz d) {
     quizService.create(d);
   }
 
+  @Operation(summary = "단일 퀴즈 조회", description = "퀴즈 아이디에 해당하는 퀴즈를 조회합니다.")
   @GetMapping("/{id}")
   public ResponseEntity<Quiz> getQuiz(@PathVariable("id") String id) {
     Quiz quiz = quizService.findById(id);
@@ -56,6 +62,7 @@ public class QuizApi {
     return new ResponseEntity<>(quizList, status);
   }
 
+  @Operation(summary = "퀴즈 목록 조회")
   @GetMapping
   public ResponseEntity<List<Quiz>> getQuizs() { // pagination 필요
     List<Quiz> quizList = quizService.findAll();
@@ -63,12 +70,14 @@ public class QuizApi {
     return new ResponseEntity<>(quizList, status);
   }
 
+  @Operation(summary = "퀴즈 업데이트")
   @PatchMapping
   @ResponseStatus(HttpStatus.OK)
   public void updateQuiz(@RequestBody Quiz d) {
     quizService.update(d);
   }
 
+  @Operation(summary = "퀴즈 삭제", description = "퀴즈 아이디에 해당하는 퀴즈를 삭제합니다.")
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
   public void deleteQuiz(@PathVariable("id") String id) {
